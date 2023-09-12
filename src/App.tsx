9ea771848d04b8d94fdf8b2f779fed03daba03ea
Juan <saraviamartin16@hotmail.com>
2023-09-12 13:38:55 -0300

    creando reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { useReducer } from 'react';
+
+//1. Create a initial State
+const initialState = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false,
+}
+
+//Create a reducer
+function reducer (state, action) {
+  const {type} = action
+  if(type === 'INTERCHANGE_LANGUAGES'){
+    return{
+      ...state,
+      fromLanguaje: state.toLanguage,
+      toLanguage: state.fromLanguaje
+    }
+  }
+  return state
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+//3. Usar UseReducer
+const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Google Translate</h1>
     </>
   )
 }
